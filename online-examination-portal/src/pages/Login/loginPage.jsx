61e3c966a42140bc417fc0b941fc5c9fac2eee3f
C-G <kelvindonye@gmail.com>
2026-05-17 02:28:56 +0100

    Testing

--- online-examination-portal/src/pages/Login/loginPage.jsx
+++ online-examination-portal/src/pages/Login/loginPage.jsx
@@ -27,84 +27,82 @@ const LoginPage = () => {
     }
   };
 
-  if (user) {
-    return navigate("/dashboard");
-  } else {
-    return (
-      <section className="h-screen w-full flex flex-col gap-10 p-5 justify-center items-center">
-        <div className="flex flex-col gap-1 justify-center items-center">
-          <h1 className="text-[#002045] text-[28px] font-bold">EduTest Pro</h1>
+  // if (user) {
+  //   return navigate("/dashboard");
+  // } else {
+  return (
+    <section className="h-screen w-full flex flex-col gap-10 p-5 justify-center items-center">
+      <div className="flex flex-col gap-1 justify-center items-center">
+        <h1 className="text-[#002045] text-[28px] font-bold">EduTest Pro</h1>
 
-          <p className="text-[#43474E] text-sm">
-            Secure Examination Environment
+        <p className="text-[#43474E] text-sm">Secure Examination Environment</p>
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-110 w-full max-h-122.75 h-full box flex flex-col gap-5"
+      >
+        <div className="border-b-2 pb-4 flex gap-2">
+          <FaLock size={20} />
+          <h2 className="text-xs font-bold">STUDENT AUTHENTICATION</h2>
+        </div>
+
+        <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+          INSTITUTIONAL EMAIL
+          <div className="border border-[#C4C6CF] h-13.5 flex gap-4 items-center p-4">
+            <IoPerson size={20} />
+            <input
+              required
+              type="text"
+              name="student-Id"
+              placeholder="Enter your ID number or Email"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, email: e.target.value }))
+              }
+              className="placeholder:text-[#6B7280] outline-none h-full w-full placeholder:text-sm"
+            />
+          </div>
+        </label>
+
+        <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+          EXAM PASSWORD
+          <div className="border border-[#C4C6CF] h-13.5 flex gap-4 items-center p-4">
+            <IoMdKey size={20} />
+            <input
+              required
+              type="password"
+              placeholder="••••••••"
+              name="student-password"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, password: e.target.value }))
+              }
+              className="placeholder:text-[#6B7280] outline-none h-full w-full placeholder:text-sm"
+            />
+          </div>
+        </label>
+
+        <div className="flex gap-2 bg-[#EFF4F9] p-4">
+          <LuShieldCheck size={20} />
+          <p className="text-sm text-[#43474E]">
+            You are logging into a proctored session. IP tracking and browser
+            monitoring are active.
           </p>
         </div>
 
-        <form
-          onSubmit={handleSubmit}
-          className="max-w-110 w-full max-h-122.75 h-full box flex flex-col gap-5"
+        <button
+          type="submit"
+          className="w-full bg-[#002045] p-4 uppercase text-white text-xs font-bold"
         >
-          <div className="border-b-2 pb-4 flex gap-2">
-            <FaLock size={20} />
-            <h2 className="text-xs font-bold">STUDENT AUTHENTICATION</h2>
-          </div>
+          Sign in to exam
+        </button>
 
-          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-            INSTITUTIONAL EMAIL
-            <div className="border border-[#C4C6CF] h-13.5 flex gap-4 items-center p-4">
-              <IoPerson size={20} />
-              <input
-                required
-                type="text"
-                name="student-Id"
-                placeholder="Enter your ID number or Email"
-                onChange={(e) =>
-                  setUserData((prev) => ({ ...prev, email: e.target.value }))
-                }
-                className="placeholder:text-[#6B7280] outline-none h-full w-full placeholder:text-sm"
-              />
-            </div>
-          </label>
-
-          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-            EXAM PASSWORD
-            <div className="border border-[#C4C6CF] h-13.5 flex gap-4 items-center p-4">
-              <IoMdKey size={20} />
-              <input
-                required
-                type="password"
-                placeholder="••••••••"
-                name="student-password"
-                onChange={(e) =>
-                  setUserData((prev) => ({ ...prev, password: e.target.value }))
-                }
-                className="placeholder:text-[#6B7280] outline-none h-full w-full placeholder:text-sm"
-              />
-            </div>
-          </label>
-
-          <div className="flex gap-2 bg-[#EFF4F9] p-4">
-            <LuShieldCheck size={20} />
-            <p className="text-sm text-[#43474E]">
-              You are logging into a proctored session. IP tracking and browser
-              monitoring are active.
-            </p>
-          </div>
-
-          <button
-            type="submit"
-            className="w-full bg-[#002045] p-4 uppercase text-white text-xs font-bold"
-          >
-            Sign in to exam
-          </button>
-
-          <p className="text-sm text-[#002045] text-center">
-            Technical support or forgot password?
-          </p>
-        </form>
-      </section>
-    );
-  }
+        <p className="text-sm text-[#002045] text-center">
+          Technical support or forgot password?
+        </p>
+      </form>
+    </section>
+  );
+  // }
 };
 
 export default LoginPage;
--- online-examination-portal/src/pages/Sign-up/signUpPage.jsx
+++ online-examination-portal/src/pages/Sign-up/signUpPage.jsx
@@ -33,119 +33,119 @@ const SignUpPage = () => {
     }
   };
 
-  if (user) {
-    return navigate("/dashboard");
-  } else {
-    return (
-      <section className="h-screen w-full flex flex-col gap-7 p-5 justify-center items-center">
-        <div className="flex flex-col gap-1 justify-center items-center">
-          <h1 className="text-[#002045] text-[28px] font-bold">
-            Create Your Account
-          </h1>
+  // if (user) {
+  //   return navigate("/dashboard");
+  // } else {
+  return (
+    <section className="h-screen w-full flex flex-col gap-7 p-5 justify-center items-center">
+      <div className="flex flex-col gap-1 justify-center items-center">
+        <h1 className="text-[#002045] text-[28px] font-bold">
+          Create Your Account
+        </h1>
 
-          <p className="text-[#43474E] text-sm">
-            Access your academic assessments and track performance.
-          </p>
-        </div>
-
-        <form
-          onSubmit={handleSubmit}
-          className="max-w-110 w-full max-h-139.25 h-full box flex flex-col gap-4"
-        >
-          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-            FULL NAME
-            <input
-              required
-              type="text"
-              name="student-full-name"
-              placeholder="John Doe"
-              onChange={(e) =>
-                setUserData((prev) => ({ ...prev, name: e.target.value }))
-              }
-              className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
-            />
-          </label>
-
-          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-            STUDENT ID
-            <input
-              required
-              type="number"
-              name="student-Id"
-              placeholder="Enter your ID number"
-              onChange={(e) =>
-                setUserData((prev) => ({ ...prev, userId: e.target.value }))
-              }
-              className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
-            />
-          </label>
-
-          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-            INSTITUTIONAL EMAIL
-            <input
-              required
-              type="text"
-              name="student-Id"
-              placeholder="j.doe@university.edu"
-              onChange={(e) =>
-                setUserData((prev) => ({ ...prev, email: e.target.value }))
-              }
-              className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
-            />
-          </label>
-
-          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-            DEPARTMENT
-            <select
-              defaultValue="computer-science"
-              onChange={(e) =>
-                setUserData((prev) => ({ ...prev, department: e.target.value }))
-              }
-              className="border border-[#C4C6CF] h-9.5 pl-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
-            >
-              <option value="computer-science">Computer Science</option>
-              <option value="information-technology">
-                Information Technology
-              </option>
-            </select>
-          </label>
-
-          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-            EXAM PASSWORD
-            <input
-              required
-              type="password"
-              placeholder="••••••••"
-              name="student-password"
-              onChange={(e) =>
-                setUserData((prev) => ({ ...prev, password: e.target.value }))
-              }
-              className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
-            />
-          </label>
-
-          <label className="flex gap-2 items-center text-sm text-[#43474E] font-normal">
-            <input type="checkbox" /> I agree to the Terms of Service and
-            Privacy Policy.
-          </label>
-
-          <button
-            type="submit"
-            className="max-h-14 h-full w-full bg-[#002045] p-4 text-white font-bold"
-          >
-            Sign Up
-          </button>
-        </form>
-
-        <p className="text-sm">
-          Already have an account?{" "}
-          <Link to={"/login"} className="text-[#002045]">
-            Log in
-          </Link>
+        <p className="text-[#43474E] text-sm">
+          Access your academic assessments and track performance.
         </p>
-      </section>
-    );
-  }
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-110 w-full max-h-139.25 h-full box flex flex-col gap-4"
+      >
+        <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+          FULL NAME
+          <input
+            required
+            type="text"
+            name="student-full-name"
+            placeholder="John Doe"
+            onChange={(e) =>
+              setUserData((prev) => ({ ...prev, name: e.target.value }))
+            }
+            className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+          />
+        </label>
+
+        <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+          STUDENT ID
+          <input
+            required
+            type="number"
+            name="student-Id"
+            placeholder="Enter your ID number"
+            onChange={(e) =>
+              setUserData((prev) => ({ ...prev, userId: e.target.value }))
+            }
+            className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+          />
+        </label>
+
+        <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+          INSTITUTIONAL EMAIL
+          <input
+            required
+            type="text"
+            name="student-Id"
+            placeholder="j.doe@university.edu"
+            onChange={(e) =>
+              setUserData((prev) => ({ ...prev, email: e.target.value }))
+            }
+            className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+          />
+        </label>
+
+        <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+          DEPARTMENT
+          <select
+            defaultValue="computer-science"
+            onChange={(e) =>
+              setUserData((prev) => ({ ...prev, department: e.target.value }))
+            }
+            className="border border-[#C4C6CF] h-9.5 pl-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+          >
+            <option value="computer-science">Computer Science</option>
+            <option value="information-technology">
+              Information Technology
+            </option>
+          </select>
+        </label>
+
+        <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+          EXAM PASSWORD
+          <input
+            required
+            type="password"
+            placeholder="••••••••"
+            name="student-password"
+            onChange={(e) =>
+              setUserData((prev) => ({ ...prev, password: e.target.value }))
+            }
+            className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+          />
+        </label>
+
+        <label className="flex gap-2 items-center text-sm text-[#43474E] font-normal">
+          <input type="checkbox" /> I agree to the Terms of Service and Privacy
+          Policy.
+        </label>
+
+        <button
+          type="submit"
+          className="max-h-14 h-full w-full bg-[#002045] p-4 text-white font-bold"
+        >
+          Sign Up
+        </button>
+      </form>
+
+      <p className="text-sm">
+        Already have an account?{" "}
+        <Link to={"/login"} className="text-[#002045]">
+          Log in
+        </Link>
+      </p>
+    </section>
+  );
+  // }
 };
 
 export default SignUpPage;
