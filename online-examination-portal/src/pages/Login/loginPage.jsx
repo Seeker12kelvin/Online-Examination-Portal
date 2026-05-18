e45f3adb6b00a7fc2168af83ea4190113d6422e4
C-G <kelvindonye@gmail.com>
2026-05-18 20:49:31 +0100

    Added loading animations to the signUp and login page.

--- online-examination-portal/src/pages/Login/loginPage.jsx
+++ online-examination-portal/src/pages/Login/loginPage.jsx
@@ -1,7 +1,8 @@
-import { useContext } from "react";
 import { FaLock } from "react-icons/fa";
 import { IoMdKey } from "react-icons/io";
 import { IoPerson } from "react-icons/io5";
+import animation from "../../images/835.gif";
+import { useContext, useState } from "react";
 import { auth } from "../../firebase/config";
 import { LuShieldCheck } from "react-icons/lu";
 import { UserContext } from "../../components/user";
@@ -10,6 +11,8 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginPage = () => {
   const { userData, setUserData } = useContext(UserContext);
+  const [loginText, setLoginText] = useState(false);
+  const [animate, setAnimate] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -17,15 +20,19 @@ const LoginPage = () => {
     const { email, password } = userData;
 
     try {
+      setAnimate(true);
       await signInWithEmailAndPassword(auth, email, password);
+      setLoginText(true);
       navigate("/dashboard");
     } catch (err) {
+      setAnimate(false);
+      setLoginText(false);
       console.error(err);
     }
   };
 
   return (
-    <main className="max-md:h-dvh md:h-screen w-full flex flex-col gap-10 p-5 justify-center items-center fixed top-0 left-0">
+    <main className="max-md:h-dvh md:h-screen w-full flex flex-col gap-10 p-5 justify-center items-center">
       <div className="flex flex-col gap-1 justify-center items-center">
         <h1 className="text-[#002045] text-[28px] font-bold">EduTest Pro</h1>
 
@@ -34,68 +41,80 @@ const LoginPage = () => {
 
       <form
         onSubmit={handleSubmit}
-        className="max-w-110 w-full max-h-122.75 h-full box flex flex-col gap-5"
+        className="max-w-110 w-full max-h-122.75 h-full box"
       >
-        <div className="border-b-2 pb-4 flex gap-2">
-          <FaLock size={20} />
-          <h2 className="text-xs font-bold">STUDENT AUTHENTICATION</h2>
-        </div>
+        {!animate ? (
+          <div className="h-fit w-full flex flex-col gap-5">
+            <div className="border-b-2 pb-4 flex gap-2">
+              <FaLock size={20} />
+              <h2 className="text-xs font-bold">STUDENT AUTHENTICATION</h2>
+            </div>
 
-        <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-          INSTITUTIONAL EMAIL
-          <div className="border border-[#C4C6CF] h-13.5 flex gap-4 items-center p-4">
-            <IoPerson size={20} />
-            <input
-              required
-              type="text"
-              name="student-email"
-              placeholder="Enter your email address"
-              onChange={(e) =>
-                setUserData((prev) => ({ ...prev, email: e.target.value }))
-              }
-              className="placeholder:text-[#6B7280] outline-none h-full w-full placeholder:text-sm"
-            />
+            <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+              INSTITUTIONAL EMAIL
+              <div className="border border-[#C4C6CF] h-13.5 flex gap-4 items-center p-4">
+                <IoPerson size={20} />
+                <input
+                  required
+                  type="text"
+                  name="student-email"
+                  placeholder="Enter your email address"
+                  onChange={(e) =>
+                    setUserData((prev) => ({ ...prev, email: e.target.value }))
+                  }
+                  className="placeholder:text-[#6B7280] outline-none h-full w-full placeholder:text-sm"
+                />
+              </div>
+            </label>
+
+            <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+              EXAM PASSWORD
+              <div className="border border-[#C4C6CF] h-13.5 flex gap-4 items-center p-4">
+                <IoMdKey size={20} />
+                <input
+                  required
+                  type="password"
+                  placeholder="••••••••"
+                  name="student-password"
+                  onChange={(e) =>
+                    setUserData((prev) => ({
+                      ...prev,
+                      password: e.target.value,
+                    }))
+                  }
+                  className="placeholder:text-[#6B7280] outline-none h-full w-full placeholder:text-sm"
+                />
+              </div>
+            </label>
+
+            <div className="flex gap-2 bg-[#EFF4F9] p-4">
+              <LuShieldCheck size={20} />
+              <p className="text-sm text-[#43474E]">
+                You are logging into a proctored session. IP tracking and
+                browser monitoring are active.
+              </p>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-[#002045] p-4 uppercase text-white text-xs font-bold"
+            >
+              Sign in to exam
+            </button>
+
+            <p className="text-sm text-[#002045] text-center">
+              Don't have an account?{" "}
+              <Link to={"/"} className="text-[#002045]">
+                Sign Up
+              </Link>
+            </p>
           </div>
-        </label>
-
-        <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-          EXAM PASSWORD
-          <div className="border border-[#C4C6CF] h-13.5 flex gap-4 items-center p-4">
-            <IoMdKey size={20} />
-            <input
-              required
-              type="password"
-              placeholder="••••••••"
-              name="student-password"
-              onChange={(e) =>
-                setUserData((prev) => ({ ...prev, password: e.target.value }))
-              }
-              className="placeholder:text-[#6B7280] outline-none h-full w-full placeholder:text-sm"
-            />
+        ) : (
+          <div className="h-full w-full flex justify-center items-center bg-[#ffffff49]">
+            <img src={animation} alt="Loading..." className="size-25" />
+            {loginText && <p>Login Successful...</p>}
           </div>
-        </label>
-
-        <div className="flex gap-2 bg-[#EFF4F9] p-4">
-          <LuShieldCheck size={20} />
-          <p className="text-sm text-[#43474E]">
-            You are logging into a proctored session. IP tracking and browser
-            monitoring are active.
-          </p>
-        </div>
-
-        <button
-          type="submit"
-          className="w-full bg-[#002045] p-4 uppercase text-white text-xs font-bold"
-        >
-          Sign in to exam
-        </button>
-
-        <p className="text-sm text-[#002045] text-center">
-          Don't have an account?{" "}
-          <Link to={"/"} className="text-[#002045]">
-            Sign Up
-          </Link>
-        </p>
+        )}
       </form>
     </main>
   );
--- online-examination-portal/src/pages/Sign-up/signUpPageForm.jsx
+++ online-examination-portal/src/pages/Sign-up/signUpPageForm.jsx
@@ -1,5 +1,6 @@
 import { auth } from "../../firebase/config";
 import { useContext, useState } from "react";
+import animation from "../../images/835.gif";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../components/user";
 // import { registerUser } from "../../firebase/firestore";
@@ -8,7 +9,9 @@ import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
 
 const SignUpPageForm = () => {
   const { userData, setUserData } = useContext(UserContext);
+  const [signUpText, setSignUpText] = useState(false);
   const [passVis, setPassVis] = useState(false);
+  const [animate, setAnimate] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -16,6 +19,8 @@ const SignUpPageForm = () => {
     const { email, password } = userData;
 
     try {
+      setAnimate(true);
+
       // This is for registering the users' email and password
       const userCrendentials = await createUserWithEmailAndPassword(
         auth,
@@ -26,8 +31,11 @@ const SignUpPageForm = () => {
 
       // This is calling the registerUser function
       // registerUser(name, email, userId, password, department);
+      setSignUpText(true);
       navigate("/login");
     } catch (err) {
+      setAnimate(false);
+      setSignUpText(false);
       console.error(err);
     }
   };
@@ -35,102 +43,114 @@ const SignUpPageForm = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="max-w-110 w-full max-h-139.25 h-full box flex flex-col gap-4"
+      className="max-w-110 w-full max-h-139.25 h-full box"
     >
-      <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-        FULL NAME
-        <input
-          required
-          type="text"
-          name="student-full-name"
-          placeholder="John Doe"
-          onChange={(e) =>
-            setUserData((prev) => ({ ...prev, name: e.target.value }))
-          }
-          className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
-        />
-      </label>
+      {!animate ? (
+        <div className="h-fit w-full flex flex-col gap-4">
+          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+            FULL NAME
+            <input
+              required
+              type="text"
+              name="student-full-name"
+              placeholder="John Doe"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, name: e.target.value }))
+              }
+              className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+            />
+          </label>
 
-      <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-        STUDENT ID
-        <input
-          required
-          type="number"
-          name="student-Id"
-          placeholder="Enter your ID number"
-          onChange={(e) =>
-            setUserData((prev) => ({ ...prev, userId: e.target.value }))
-          }
-          className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
-        />
-      </label>
+          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+            STUDENT ID
+            <input
+              required
+              type="number"
+              name="student-Id"
+              placeholder="Enter your ID number"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, userId: e.target.value }))
+              }
+              className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+            />
+          </label>
 
-      <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-        INSTITUTIONAL EMAIL
-        <input
-          required
-          type="text"
-          name="student-email"
-          placeholder="jodoe@gmail.com"
-          onChange={(e) =>
-            setUserData((prev) => ({ ...prev, email: e.target.value }))
-          }
-          className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
-        />
-      </label>
+          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+            INSTITUTIONAL EMAIL
+            <input
+              required
+              type="text"
+              name="student-email"
+              placeholder="jodoe@gmail.com"
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, email: e.target.value }))
+              }
+              className="border border-[#C4C6CF] h-9.5 p-4 placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+            />
+          </label>
 
-      <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-        DEPARTMENT
-        <div className="border border-[#C4C6CF] h-9.5 px-4 flex items-center">
-          <select
-            defaultValue="computer-science"
-            onChange={(e) =>
-              setUserData((prev) => ({ ...prev, department: e.target.value }))
-            }
-            className="h-full placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+            DEPARTMENT
+            <div className="border border-[#C4C6CF] h-9.5 px-4 flex items-center">
+              <select
+                defaultValue="computer-science"
+                onChange={(e) =>
+                  setUserData((prev) => ({
+                    ...prev,
+                    department: e.target.value,
+                  }))
+                }
+                className="h-full placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+              >
+                <option value="computer-science">Computer Science</option>
+                <option value="information-technology">
+                  Information Technology
+                </option>
+              </select>
+            </div>
+          </label>
+
+          <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
+            EXAM PASSWORD
+            <div className="border border-[#C4C6CF] h-9.5 px-4 flex justify-between items-center">
+              <input
+                required
+                type={passVis ? "text" : "password"}
+                placeholder="••••••••"
+                name="student-password"
+                onChange={(e) =>
+                  setUserData((prev) => ({ ...prev, password: e.target.value }))
+                }
+                className="h-full placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
+              />
+              <button type="button" onClick={() => setPassVis((prev) => !prev)}>
+                {passVis ? (
+                  <IoEyeOutline size={20} />
+                ) : (
+                  <IoEyeOffOutline size={20} />
+                )}
+              </button>
+            </div>
+          </label>
+
+          <label className="flex gap-2 items-center text-sm text-[#43474E] font-normal max-md:text-xs">
+            <input type="checkbox" /> I agree to the Terms of Service and
+            Privacy Policy.
+          </label>
+
+          <button
+            type="submit"
+            className="max-h-14 h-full w-full bg-[#002045] p-4 text-white font-bold"
           >
-            <option value="computer-science">Computer Science</option>
-            <option value="information-technology">
-              Information Technology
-            </option>
-          </select>
-        </div>
-      </label>
-
-      <label className="text-[#43474E] font-bold text-xs flex flex-col gap-2">
-        EXAM PASSWORD
-        <div className="border border-[#C4C6CF] h-9.5 px-4 flex justify-between items-center">
-          <input
-            required
-            type={passVis ? "text" : "password"}
-            placeholder="••••••••"
-            name="student-password"
-            onChange={(e) =>
-              setUserData((prev) => ({ ...prev, password: e.target.value }))
-            }
-            className="h-full placeholder:text-[#6B7280] outline-none w-full placeholder:text-sm"
-          />
-          <button type="button" onClick={() => setPassVis((prev) => !prev)}>
-            {passVis ? (
-              <IoEyeOutline size={20} />
-            ) : (
-              <IoEyeOffOutline size={20} />
-            )}
+            Sign Up
           </button>
         </div>
-      </label>
-
-      <label className="flex gap-2 items-center text-sm text-[#43474E] font-normal max-md:text-xs">
-        <input type="checkbox" /> I agree to the Terms of Service and Privacy
-        Policy.
-      </label>
-
-      <button
-        type="submit"
-        className="max-h-14 h-full w-full bg-[#002045] p-4 text-white font-bold"
-      >
-        Sign Up
-      </button>
+      ) : (
+        <div className="h-full w-full flex flex-col gap-3 justify-center items-center bg-[#ffffff49]">
+          <img src={animation} alt="Loading..." className="size-25" />
+          {signUpText && <p>Sign Up Successful...</p>}
+        </div>
+      )}
     </form>
   );
 };
